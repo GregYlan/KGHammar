eac7c240749e470ced90c1cc8577c2df42e8d6cf
Ylan <ylan@ylan.nu>
2015-06-13 19:40:01 +0200

    Initial Commit

---
+++ Ti Mossan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ti Mossan
+//
+//  Created by Ylan Sthlm on 2015-06-13.
+//  Copyright © 2015 Ylan Sthlm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Ti MossanTests/Ti_MossanTests.swift
@@ -0,0 +1,35 @@
+//
+//  Ti_MossanTests.swift
+//  Ti MossanTests
+//
+//  Created by Ylan Sthlm on 2015-06-13.
+//  Copyright © 2015 Ylan Sthlm. All rights reserved.
+//
+
+import XCTest
+
+class Ti_MossanTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
